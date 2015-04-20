10a3b236d9e607aa9b35a92349a5cb323d344719
Anna Kennedy <yedennek@googlemail.com>
2015-04-20 14:17:18 +0100

    Add method to post updated state

--- lib/light-control.rb
+++ lib/light-control.rb
@@ -1,17 +1,25 @@
 require "sinatra"
 require "json"
 
+rooms = { rooms:
+    [
+      {id: "room-1", room: "Office",  on: true,    colour: "#ff0000"},
+      {id: "room-2", room: "Porch",   on: false,   colour: "#ff66ff"},
+      {id: "room-3", room: "Kitchen", on: true,    colour: "#0066ff"},
+    ]
+  }
+
 get '/' do
   haml :index
 end
 
 get "/rooms" do
   content_type :json
-  { rooms:
-    [
-      {id: "room-1", room: "Office",  on: true,    colour: "#ff0000"},
-      {id: "room-2", room: "Porch",   on: false,   colour: "#ff66ff"},
-      {id: "room-3", room: "Kitchen", on: true,    colour: "#0066ff"},
-    ]
-  }.to_json
+  rooms.to_json
+end
+
+patch "/rooms" do
+  request.body.rewind
+  data = JSON.parse request.body.read
+  rooms = { rooms: data }
 end
